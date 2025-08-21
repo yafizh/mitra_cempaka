bed09382a41e98956f0299ee41f8319d05a05de4
yafizh <a.chairil.yafizh@gmail.com>
2025-08-21 19:49:14 +0700

    cashier page with cart provider

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,28 @@
+import 'dart:collection';
+
+import 'package:flutter/widgets.dart';
+import 'package:mitra_cempaka/models/drug.dart';
+
+class CartModel extends ChangeNotifier {
+  final List<Drug> _drugs = [];
+
+  UnmodifiableListView<Drug> get items => UnmodifiableListView(_drugs);
+
+  int get totalDrug => _drugs.length;
+  int get totalDrugPrice => _drugs.fold(0, (sum, drug) => sum + drug.price);
+
+  void add(Drug drug) {
+    _drugs.add(drug);
+    notifyListeners();
+  }
+
+  void remove(Drug drug) {
+    _drugs.remove(drug);
+    notifyListeners();
+  }
+
+  void removeAll() {
+    _drugs.clear();
+    notifyListeners();
+  }
+}
--- lib/pages/cashier_page.dart
+++ lib/pages/cashier_page.dart
@@ -2,7 +2,9 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:mitra_cempaka/models/cart_model.dart';
 import 'package:mitra_cempaka/models/drug.dart';
+import 'package:provider/provider.dart';
 
 class CashierPage extends StatelessWidget {
   CashierPage({super.key});
@@ -15,87 +17,105 @@ class CashierPage extends StatelessWidget {
       drugs.add(Drug("Obat $i", Random().nextInt(100) + 10000));
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Cashier", style: TextStyle(fontWeight: FontWeight.bold)),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              // Navigator.of(
-              //   context,
-              // ).push(MaterialPageRoute(builder: (context) => const CartPage()));
-            },
-            icon: Icon(Icons.shopping_cart_outlined),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.all(15),
-                hintText: 'Search Item',
-                hintStyle: const TextStyle(color: Colors.grey, fontSize: 14),
-                suffixIcon: SizedBox(
-                  width: 100,
-                  child: IntrinsicHeight(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        const VerticalDivider(
-                          color: Colors.black,
-                          indent: 10,
-                          endIndent: 10,
-                          thickness: 0.4,
-                        ),
-                        IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-                      ],
+    return ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Cashier", style: TextStyle(fontWeight: FontWeight.bold)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Navigator.of(
+                //   context,
+                // ).push(MaterialPageRoute(builder: (context) => const CartPage()));
+              },
+              icon: Icon(Icons.shopping_cart_outlined),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.all(15),
+                  hintText: 'Search Item',
+                  hintStyle: const TextStyle(color: Colors.grey, fontSize: 14),
+                  suffixIcon: SizedBox(
+                    width: 100,
+                    child: IntrinsicHeight(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const VerticalDivider(
+                            color: Colors.black,
+                            indent: 10,
+                            endIndent: 10,
+                            thickness: 0.4,
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.search),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 16),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ListView.builder(
-                itemCount: drugs.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    // margin: EdgeInsets.all(0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: ListTile(
-                      title: Text(drugs[index].name),
-                      subtitle: Text(
-                        NumberFormat.currency(
-                          locale: 'id_ID',
-                          symbol: 'Rp ',
-                          decimalDigits: 0,
-                        ).format(drugs[index].price),
-                      ),
-                      trailing: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.add_shopping_cart_sharp),
-                      ),
-                    ),
-                  );
-                },
+            SizedBox(height: 16),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListView.builder(
+                  itemCount: drugs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Consumer<CartModel>(
+                      builder: (context, cart, child) {
+                        return Card(
+                          margin: EdgeInsets.all(0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: ListTile(
+                            title: Text(drugs[index].name),
+                            subtitle: Text(
+                              NumberFormat.currency(
+                                locale: 'id_ID',
+                                symbol: 'Rp ',
+                                decimalDigits: 0,
+                              ).format(drugs[index].price),
+                            ),
+                            trailing: IconButton(
+                              onPressed: () {
+                                cart.items.contains(drugs[index])
+                                    ? cart.remove(drugs[index])
+                                    : cart.add(drugs[index]);
+                              },
+                              icon: Icon(
+                                cart.items.contains(drugs[index])
+                                    ? Icons.remove_shopping_cart_sharp
+                                    : Icons.add_shopping_cart_sharp,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
