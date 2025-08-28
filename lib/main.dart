677d46863115be9b4f7221acc7ad3045c454be89
yafizh <a.chairil.yafizh@gmail.com>
2025-08-28 19:28:51 +0700

    move change nnotofier provider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mitra_cempaka/models/cart_model.dart';
 import 'package:mitra_cempaka/pages/splashscreen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/pages/cashier_page.dart
+++ lib/pages/cashier_page.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:mitra_cempaka/models/cart_model.dart';
 import 'package:mitra_cempaka/models/drug.dart';
+import 'package:mitra_cempaka/pages/cart_page.dart';
 import 'package:provider/provider.dart';
 
 class CashierPage extends StatefulWidget {
-  CashierPage({super.key});
+  const CashierPage({super.key});
 
   @override
   State<CashierPage> createState() => _CashierPageState();
@@ -15,7 +16,7 @@ class CashierPage extends StatefulWidget {
 class _CashierPageState extends State<CashierPage> {
   List<Drug> drugs = [];
 
-  ScrollController _scrollController = new ScrollController();
+  final ScrollController _scrollController = ScrollController();
 
   int _page = 1;
 
@@ -55,123 +56,120 @@ class _CashierPageState extends State<CashierPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CartModel(),
-      child: Consumer<CartModel>(
-        builder: (context, cart, child) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(
-                "Cashier",
-                style: TextStyle(fontWeight: FontWeight.bold),
+    return Consumer<CartModel>(
+      builder: (context, cart, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Cashier",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const CartPage()),
+                  );
+                },
+                icon: cart.totalDrug == 0
+                    ? Icon(Icons.shopping_cart_outlined)
+                    : Badge.count(
+                        count: cart.totalDrug,
+                        child: Icon(Icons.shopping_cart_outlined),
+                      ),
               ),
-              centerTitle: true,
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    // Navigator.of(
-                    //   context,
-                    // ).push(MaterialPageRoute(builder: (context) => const CartPage()));
-                  },
-                  icon: cart.totalDrug == 0
-                      ? Icon(Icons.shopping_cart_outlined)
-                      : Badge.count(
-                          count: cart.totalDrug,
-                          child: Icon(Icons.shopping_cart_outlined),
-                        ),
-                ),
-              ],
-            ),
-            body: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: const EdgeInsets.all(15),
-                      hintText: 'Search Item',
-                      hintStyle: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14,
-                      ),
-                      suffixIcon: SizedBox(
-                        width: 100,
-                        child: IntrinsicHeight(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              const VerticalDivider(
-                                color: Colors.black,
-                                indent: 10,
-                                endIndent: 10,
-                                thickness: 0.4,
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.search),
-                              ),
-                            ],
-                          ),
+            ],
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: const EdgeInsets.all(15),
+                    hintText: 'Search Item',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                    suffixIcon: SizedBox(
+                      width: 100,
+                      child: IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            const VerticalDivider(
+                              color: Colors.black,
+                              indent: 10,
+                              endIndent: 10,
+                              thickness: 0.4,
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.search),
+                            ),
+                          ],
                         ),
                       ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: ListView.builder(
-                      controller: _scrollController,
-                      itemCount: (drugs.length + (_page == _totalPage ? 0 : 1)),
-                      itemBuilder: (BuildContext context, int index) {
-                        if (index == drugs.length) {
-                          return Padding(
-                            padding: EdgeInsetsGeometry.symmetric(vertical: 16),
-                            child: Center(child: CircularProgressIndicator()),
-                          );
-                        }
-
-                        return Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          child: ListTile(
-                            title: Text(drugs[index].name),
-                            subtitle: Text(
-                              NumberFormat.currency(
-                                locale: 'id_ID',
-                                symbol: 'Rp ',
-                                decimalDigits: 0,
-                              ).format(drugs[index].price),
-                            ),
-                            trailing: IconButton(
-                              onPressed: () {
-                                cart.items.contains(drugs[index])
-                                    ? cart.remove(drugs[index])
-                                    : cart.add(drugs[index]);
-                              },
-                              icon: Icon(
-                                cart.items.contains(drugs[index])
-                                    ? Icons.remove_shopping_cart_sharp
-                                    : Icons.add_shopping_cart_sharp,
-                              ),
-                            ),
-                          ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ListView.builder(
+                    controller: _scrollController,
+                    itemCount: (drugs.length + (_page == _totalPage ? 0 : 1)),
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index == drugs.length) {
+                        return Padding(
+                          padding: EdgeInsetsGeometry.symmetric(vertical: 16),
+                          child: Center(child: CircularProgressIndicator()),
                         );
-                      },
-                    ),
+                      }
+
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: ListTile(
+                          title: Text(drugs[index].name),
+                          subtitle: Text(
+                            NumberFormat.currency(
+                              locale: 'id_ID',
+                              symbol: 'Rp ',
+                              decimalDigits: 0,
+                            ).format(drugs[index].price),
+                          ),
+                          trailing: IconButton(
+                            onPressed: () {
+                              cart.drugs.contains(drugs[index])
+                                  ? cart.remove(drugs[index])
+                                  : cart.add(drugs[index]);
+                            },
+                            icon: Icon(
+                              cart.drugs.contains(drugs[index])
+                                  ? Icons.remove_shopping_cart_sharp
+                                  : Icons.add_shopping_cart_sharp,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
-              ],
-            ),
-          );
-        },
-      ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
